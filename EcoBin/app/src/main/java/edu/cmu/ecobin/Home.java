fb3990e822396d55fd90628fc6ac38a2f4abd67b
Yilun Xu <yilunx@andrew.cmu.edu>
2018-02-22 22:47:00 -0500

    first commit with menu

---
+++ EcoBin/app/src/main/java/edu/cmu/ecobin/Home.java
@@ -0,0 +1,28 @@
+package edu.cmu.ecobin;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Onedollar on 2/22/18.
+ */
+
+public class Home extends Fragment {
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        getActivity().setTitle("Home");
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.home, container, false);
+    }
+}
---
+++ EcoBin/app/src/main/java/edu/cmu/ecobin/Score.java
@@ -0,0 +1,28 @@
+package edu.cmu.ecobin;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Onedollar on 2/22/18.
+ */
+
+public class Score extends Fragment {
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        getActivity().setTitle("Score Board");
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.score, container, false);
+    }
+}
