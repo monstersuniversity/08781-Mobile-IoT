1027c331bc46ae5e538177c939ee6e0db1463247
Yilun Xu <yilunx@andrew.cmu.edu>
2018-02-24 22:23:37 -0500

    ui first version

--- EcoBin/app/src/main/java/edu/cmu/ecobin/History.java
+++ EcoBin/app/src/main/java/edu/cmu/ecobin/History.java
@@ -18,9 +18,9 @@ import org.eazegraph.lib.models.PieModel;
  */
 
 public class History extends Fragment {
-    BarChart mBarChart1;
-    BarChart mBarChart2;
-    PieChart mPieChart;
+    BarChart mBarChart_month;
+    BarChart mBarChart_year;
+
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
@@ -32,41 +32,37 @@ public class History extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.history, container, false);
-        mBarChart1 = view.findViewById(R.id.barchart);
+        mBarChart_month = view.findViewById(R.id.barchart_month);
 
-        mBarChart1.addBar(new BarModel(2.3f, 0xFF123456));
-        mBarChart1.addBar(new BarModel(2.f,  0xFF343456));
-        mBarChart1.addBar(new BarModel(3.3f, 0xFF563456));
-        mBarChart1.addBar(new BarModel(1.1f, 0xFF873F56));
-        mBarChart1.addBar(new BarModel(2.7f, 0xFF56B7F1));
-        mBarChart1.addBar(new BarModel(2.f,  0xFF343456));
-        mBarChart1.addBar(new BarModel(0.4f, 0xFF1FF4AC));
-        mBarChart1.addBar(new BarModel(4.f,  0xFF1BA4E6));
+        mBarChart_month.addBar(new BarModel("week 12", 43.3f, 0xFFEF25E8));
+        mBarChart_month.addBar(new BarModel("week 13", 45.f,  0xFF9768E8));
+        mBarChart_month.addBar(new BarModel("week 14", 24.3f, 0xFF8A9CF4));
+        mBarChart_month.addBar(new BarModel("week 15", 34.1f, 0xFF719DEF));
+        mBarChart_month.addBar(new BarModel("week 16", 25.7f, 0xFF82C0AC));
+        mBarChart_month.addBar(new BarModel("week 17", 45.f,  0xFFB4DB84));
+        mBarChart_month.addBar(new BarModel("week 18", 35.4f, 0xFFE5EF9F));
+        mBarChart_month.addBar(new BarModel("week 19", 47.f,  0xFFFBF4A2));
 
-        mBarChart1.startAnimation();
+        mBarChart_month.startAnimation();
 
 
-//        mBarChart2 = view.findViewById(R.id.barchart2);
-//
-//        mBarChart2.addBar(new BarModel(2.3f, 0xFF123456));
-//        mBarChart2.addBar(new BarModel(2.f,  0xFF343456));
-//        mBarChart2.addBar(new BarModel(3.3f, 0xFF563456));
-//        mBarChart2.addBar(new BarModel(1.1f, 0xFF873F56));
-//        mBarChart2.addBar(new BarModel(2.7f, 0xFF56B7F1));
-//        mBarChart2.addBar(new BarModel(2.f,  0xFF343456));
-//        mBarChart2.addBar(new BarModel(0.4f, 0xFF1FF4AC));
-//        mBarChart2.addBar(new BarModel(4.f,  0xFF1BA4E6));
-//
-//        mBarChart2.startAnimation();
+        mBarChart_year = view.findViewById(R.id.barchart_year);
 
-        mPieChart = view.findViewById(R.id.piechart);
+        mBarChart_year.addBar(new BarModel("Jan", 45.f,  0xFFFB9289));
+        mBarChart_year.addBar(new BarModel("Feb", 35.f,  0xFFFAAA80));
+        mBarChart_year.addBar(new BarModel("Mar", 46.3f, 0xFFF9C576));
+        mBarChart_year.addBar(new BarModel("Apr", 64.1f, 0xFFFAE494));
+        mBarChart_year.addBar(new BarModel("May", 34.7f, 0xFFFBF4A2));
+        mBarChart_year.addBar(new BarModel("June", 45.f, 0xFFE5EF9F));
+        mBarChart_year.addBar(new BarModel("Jul", 36.4f, 0xFFB4DB84));
+        mBarChart_year.addBar(new BarModel("Aug", 57.f,  0xFF82C0AC));
+        mBarChart_year.addBar(new BarModel("Sept", 57.f, 0xFF719DEF));
+        mBarChart_year.addBar(new BarModel("Oct", 57.f,  0xFF8A9CF4));
+        mBarChart_year.addBar(new BarModel("Nov", 57.f,  0xFF9768E8));
+        mBarChart_year.addBar(new BarModel("Dec", 57.f,  0xFFEF25E8));
 
-        mPieChart.addPieSlice(new PieModel("Freetime", 15, Color.parseColor("#FE6DA8")));
-        mPieChart.addPieSlice(new PieModel("Sleep", 25, Color.parseColor("#56B7F1")));
-        mPieChart.addPieSlice(new PieModel("Work", 35, Color.parseColor("#CDA67F")));
-        mPieChart.addPieSlice(new PieModel("Eating", 9, Color.parseColor("#FED70E")));
 
-        mPieChart.startAnimation();
+        mBarChart_year.startAnimation();
 
 
 
--- EcoBin/app/src/main/java/edu/cmu/ecobin/Home.java
+++ EcoBin/app/src/main/java/edu/cmu/ecobin/Home.java
@@ -23,6 +23,8 @@ import com.github.mikephil.charting.data.PieEntry;
 import com.github.mikephil.charting.highlight.Highlight;
 import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
 
+import org.eazegraph.lib.models.PieModel;
+
 import java.util.ArrayList;
 
 /**
@@ -31,11 +33,13 @@ import java.util.ArrayList;
 
 public class Home extends Fragment {
 
-    private static String TAG = "MainActivity";
+    org.eazegraph.lib.charts.PieChart mPieChart;
 
-    private float[] yData = {20f, 80f};
-    private String[] xData = {"Trash", "Recycle"};
-    PieChart pieChart;
+//    private static String TAG = "MainActivity";
+//
+//    private float[] yData = {20f, 80f};
+//    private String[] xData = {"Trash", "Recycle"};
+//    PieChart pieChart;
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
@@ -45,73 +49,75 @@ public class Home extends Fragment {
 
     }
 
-    private void addDataSet() {
-        Log.d(TAG, "addDataSet started");
-        ArrayList<PieEntry> yEntrys = new ArrayList<>();
-        ArrayList<String> xEntrys = new ArrayList<>();
-
-        for(int i = 0; i < yData.length; i++){
-            yEntrys.add(new PieEntry(yData[i] , i));
-        }
-
-        for(int i = 1; i < xData.length; i++){
-            xEntrys.add(xData[i]);
-        }
-
-        //create the data set
-        PieDataSet pieDataSet = new PieDataSet(yEntrys, "Trash and Recycle");
-        pieDataSet.setSliceSpace(2);
-        pieDataSet.setValueTextSize(12);
-
-        //add colors to dataset
-        ArrayList<Integer> colors = new ArrayList<>();
-//        colors.add(Color.GRAY);
-//        colors.add(Color.BLUE);
-        colors.add(Color.argb(100, 138,43,226));
-        colors.add(Color.argb(100, 230,230,250));
-//        colors.add(Color.CYAN);
-//        colors.add(Color.YELLOW);
-//        colors.add(Color.MAGENTA);
-
-        pieDataSet.setColors(colors);
-
-        //add legend to chart
-        Legend legend = pieChart.getLegend();
-        legend.setForm(Legend.LegendForm.CIRCLE);
-        legend.setPosition(Legend.LegendPosition.LEFT_OF_CHART);
-
-        //create pie data object
-        PieData pieData = new PieData(pieDataSet);
-        pieChart.setData(pieData);
-        pieChart.invalidate();
-    }
+//    private void addDataSet() {
+//        Log.d(TAG, "addDataSet started");
+//        ArrayList<PieEntry> yEntrys = new ArrayList<>();
+//        ArrayList<String> xEntrys = new ArrayList<>();
+//
+//        for(int i = 0; i < yData.length; i++){
+//            yEntrys.add(new PieEntry(yData[i] , i));
+//        }
+//
+//        for(int i = 1; i < xData.length; i++){
+//            xEntrys.add(xData[i]);
+//        }
+//
+//        //create the data set
+//        PieDataSet pieDataSet = new PieDataSet(yEntrys, "Trash and Recycle");
+//        pieDataSet.setSliceSpace(2);
+//        pieDataSet.setValueTextSize(12);
+//
+//        //add colors to dataset
+//        ArrayList<Integer> colors = new ArrayList<>();
+//        colors.add(Color.argb(100, 138,43,226));
+//        colors.add(Color.argb(100, 230,230,250));
+//
+//        pieDataSet.setColors(colors);
+//
+//        //add legend to chart
+//        Legend legend = pieChart.getLegend();
+//        legend.setForm(Legend.LegendForm.CIRCLE);
+//        legend.setPosition(Legend.LegendPosition.LEFT_OF_CHART);
+//
+//        //create pie data object
+//        PieData pieData = new PieData(pieDataSet);
+//        pieChart.setData(pieData);
+//        pieChart.invalidate();
+//    }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.home, container, false);
 
-        Log.d(TAG, "onCreate: starting to create chart");
 
-        pieChart = (PieChart)rootView.findViewById(R.id.idPieChart);
+        mPieChart = rootView.findViewById(R.id.piechart);
 
-        Description des = new Description();
-        des.setText("Your Recycle rate");
-        pieChart.setDescription(des);
-        //pieChart.setDescription("Sales by employee (In Thousands $)");
-        pieChart.setRotationEnabled(true);
-        //pieChart.setUsePercentValues(true);
-        //pieChart.setHoleColor(Color.BLUE);
-        //pieChart.setCenterTextColor(Color.BLACK);
-        pieChart.setHoleRadius(25f);
-        pieChart.setTransparentCircleAlpha(0);
-        pieChart.setCenterText("Recycle Rate");
-        pieChart.setCenterTextSize(10);
-        //pieChart.setDrawEntryLabels(true);
-        //pieChart.setEntryLabelTextSize(20);
-        //More options just check out the documentation!
+        mPieChart.addPieSlice(new PieModel("Recycle Percentage", 32, Color.parseColor("#FE6DA8")));
+        mPieChart.addPieSlice(new PieModel("Trash Percentage", 68, Color.parseColor("#56B7F1")));
+        mPieChart.startAnimation();
 
-        addDataSet();
+//        Log.d(TAG, "onCreate: starting to create chart");
+//
+//        pieChart = (PieChart)rootView.findViewById(R.id.idPieChart);
+//
+//        Description des = new Description();
+//        des.setText("Your Recycle rate");
+//        pieChart.setDescription(des);
+//        //pieChart.setDescription("Sales by employee (In Thousands $)");
+//        pieChart.setRotationEnabled(true);
+//        //pieChart.setUsePercentValues(true);
+//        //pieChart.setHoleColor(Color.BLUE);
+//        //pieChart.setCenterTextColor(Color.BLACK);
+//        pieChart.setHoleRadius(25f);
+//        pieChart.setTransparentCircleAlpha(0);
+//        pieChart.setCenterText("Recycle Rate");
+//        pieChart.setCenterTextSize(10);
+//        //pieChart.setDrawEntryLabels(true);
+//        //pieChart.setEntryLabelTextSize(20);
+//        //More options just check out the documentation!
+//
+//        addDataSet();
 
         return rootView;
     }
